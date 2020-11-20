8951a167580cb1736ee27797e4bbacbca6912e01
unknown <juc096@ucsd.edu>
2020-11-19 17:50:30 -0800

    complete

--- test_java_q/app/src/main/java/com/example/test_java_q/java_test_part2.java
+++ test_java_q/app/src/main/java/com/example/test_java_q/java_test_part2.java
@@ -1,11 +1,20 @@
 package com.example.test_java_q;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Set;
+import java.util.SortedSet;
 import java.util.Stack;
+import java.util.TreeMap;
+import java.util.Map;
+import java.util.TreeSet;
 
 public class java_test_part2 {
     //9) program to print first non repeated character from String
@@ -95,25 +104,130 @@ public class java_test_part2 {
         System.out.println("consonant count: " + consonant_count);
     }
     //12
-    //1min
+    //time: 1min
     public String replace_char(String s1, char c, char r) {
         return s1.replace(c, r);
     }
-    //13
-    //not finished
-    Set<String> perms = new HashSet<>();
-    public void permutations(String s) {
-        //base cases
-        if (s.length() == 1) {
-            perms.add(s);
-            return;
-        }
-        if (s.length() == 2) {
+     //14 find continuous sub array whose sum is equal to given number
+    //time: 10min
+    public int[] find_subarray(int[] a, int k) {
 
-        Set<String> perms = new HashSet<>();
-        Stack<Character> st = new Stack<>();
-        permutations(s)
+        for (int i = 0; i < a.length; i++) {
+            int cur_sum = k;
+            for (int j = i+1; j < a.length; j++) {
+                cur_sum -= a[i];
+                if (cur_sum < 0)
+                    break;
+                if (cur_sum == 0)
+                    return Arrays.copyOfRange(a, i, j);
+            }
+
+        }
+        return null;
+    }
+
+
+        //15
+        //find intersection of two arrays in java
+        //time: 5-10min
+        public Set<Integer> find_intersection(int[] a, int[] b) {
+            Set<Integer> a1 = new HashSet<>();
+            Set<Integer> b1 = new HashSet<>();
+            for (int i = 0; i < a.length; i++)
+                a1.add(a[i]);
+            for (int i = 0; i < b.length; i++)
+                b1.add(b[i]);
+
+            a1.retainAll(b1);
+            return a1;
+
+        }
+
+        //16 separate 0s from non 0s in array
+        //time: 5-10min
+        public LinkedList<Integer> separate_zeros(int[] a) {
+
+            LinkedList<Integer> q = new LinkedList<>();
+
+            for (int i = 0; i < a.length; i++) {
+                if (a[i] != 0)
+                    q.addLast(a[i]);
+                if (a[i] == 0)
+                    q.addFirst(0);
+            }
+
+            return q;
+
+        }
+        //17 find longest substring without repeating characters in a string
+        //time: 20min
+    public String find_longest_sub(String s) {
+        LinkedHashMap<Character, Boolean> dupe_letters = new LinkedHashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            Character c = s.charAt(i);
+            Boolean visited = dupe_letters.containsKey(c);
+            dupe_letters.put(c, visited);
+        }
+        int first_i, last_i, longest;
+        first_i = last_i = longest = 0;
+
+        for (Character c : dupe_letters.keySet()) {
+            if(!dupe_letters.get(c)) {
+                int temp_first_i = s.indexOf(c);
+
+                for (int i = s.indexOf(c); i < s.length(); i++) {
+                    Character temp_c = s.charAt(c);
+
+                    if (dupe_letters.get(temp_c) == false)
+                        continue;
+
+                    if ((i - temp_first_i) > longest) {
+                            first_i = temp_first_i;
+                            last_i = i;
+                            longest = last_i - first_i;
+                    }
+                    break;
+                }
+            }
+        }
+
+        return s.substring(first_i, last_i);
 
     }
+    //18 Find the maximum value in Array without using Collection
+    //time: 1min
+    public int find_max(int a[]) {
+            int max = a[0];
+            for (int i = 0; i < a.length; i++) {
+                if (max < a[i])
+                    max = a[i];
+            }
+            return max;
+    }
+
+    //19 sort HashMap in java by keys and values
+    //time: 5-10min
+    public TreeMap<Integer,Integer> sort_map_key(HashMap<Integer,Integer> a) {
+            return new TreeMap<>(a);
+    }
+    public TreeSet<Map.Entry<Integer, Integer>> sort_map_value(HashMap<Integer, Integer> a) {
+
+            Comparator<Map.Entry<Integer, Integer>> comp =
+                (Map.Entry<Integer, Integer> b1, Map.Entry<Integer, Integer> b2) ->
+                        (b1.getValue().compareTo(b2.getValue()));
+
+            TreeSet<Map.Entry<Integer, Integer>> t = new TreeSet<>(comp);
+            t.addAll(a.entrySet());
+            return t;
+
+    }
+    //20 Write a java program to iterate over HashSet in java
+    //time: 2 min
+    public void iterate_hashset(HashSet<Integer> a) {
+        for (Iterator<Integer> it = a.iterator(); it.hasNext(); )
+            System.out.println(it.next());
+    }
+
 
 }
---
+++ test_java_q/app/src/main/java/com/example/test_java_q/permutations.java
@@ -0,0 +1,85 @@
+package com.example.test_java_q;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Set;
+import java.util.Stack;
+
+//13
+//time: 1hr
+
+public class permutations {
+
+    public LinkedList<Character> str;
+    public Set<String> all_permutations;
+
+    public permutations(String s) {
+
+        str = new LinkedList<>();
+        all_permutations = new HashSet<>();
+
+        //linkedlist to represent string
+        for (int i = 0; i < s.length(); i++)
+            str.add(s.charAt(i));
+
+    }
+    //recursive function to calculate permutations
+    //we return a stack filled with string combinations
+    private Stack<String> permute() {
+        //stack is initiated and used as string builder
+        Stack<String> st = new Stack<>();
+        //base case
+        if (str.size() == 0) {
+            st.push("\0");
+            return st;
+        }
+
+        //loop based on linkedlist size
+        //note: as we recurse down, linkedlist will shrink to size 0, then as we
+        //recurse up, size/elements will be restored
+        for (int i = 0; i < str.size(); i++) {
+            //get the first value from the top of linkedlist and pop it off
+            Character first_c = str.getFirst();
+            str.removeFirst();
+
+            //recursive call -- next recurse will operate on a smaller linked list without
+            //the first char
+            Stack<String> temp = permute();
+
+            //now we got combinations in stack, we start prepending the first char to each string
+            //in stack
+            while (!temp.empty()) {
+                String temp_s = first_c + temp.peek();
+                //all_permutations holds them all in a set
+                all_permutations.add(temp_s);
+                //stack is used as a string builder
+                st.push(temp_s);
+                temp.pop();
+            }
+            //add element back into linkedlist tail-end
+            //loop will run through all elements this way
+            str.addLast(first_c);
+        }
+
+        return st;
+    }
+
+    public Set<String> get_permutations() {
+        this.permute();
+        return all_permutations;
+    }
+
+    public static void main(String[] args) {
+        permutations perm = new permutations("abcd");
+        Set<String> perm_set = perm.get_permutations();
+        for (Iterator<String> it = perm_set.iterator(); it.hasNext();)
+            System.out.println(it.next());
+
+        //check if the size of the set is correct using some math based on a string size = 4
+        if (perm_set.size() != ((4*3*2*1) + (4*3*2) + (4*3) + (4*1)))
+            System.out.println("Something when wrong");
+
+    }
+}
+
