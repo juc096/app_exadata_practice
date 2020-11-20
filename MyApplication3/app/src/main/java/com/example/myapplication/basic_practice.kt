3e9b1ce8f13d91c7dad8beef78ea24515820ee3a
unknown <juc096@ucsd.edu>
2020-11-20 13:36:27 -0800

    kotlin pract updated

--- MyApplication3/app/src/main/java/com/example/myapplication/basic_practice.kt
+++ MyApplication3/app/src/main/java/com/example/myapplication/basic_practice.kt
@@ -7,8 +7,19 @@ fun main() {
     println("hello, please enter what n'th you'd like");
     var num = read.nextInt()
     println(fib(num))
-}
+    println(fib_rec(num))
 
+    val lambdaCompare : (Int, Int) -> Boolean = {a: Int, b: Int -> a>b}
+    val lambdaCompare_R : (Int, Int, (Int, Int) -> Boolean) -> Boolean =
+        {a: Int, b: Int, fn: (Int, Int) -> Boolean -> !fn(a,b)}
+
+
+    println(high_ord_comp(1, 2, lambdaCompare))
+
+}
+fun high_ord_comp(a: Int, b: Int, fn: (Int, Int) -> Boolean): Boolean {
+    return fn(a,b)
+}
 fun fib(a: Int): Int {
     if (a <= 1)
         return a
@@ -20,3 +31,11 @@ fun fib(a: Int): Int {
     }
     return n
 }
+
+fun fib_rec(a: Int): Int {
+    if (a <= 1)
+        return a
+    return (fib_rec(a-1) + fib_rec(a-2))
+}
+
+fun
